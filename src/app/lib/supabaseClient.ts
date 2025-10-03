7829eaa49700b0a578f03ae3f28be06a3d957fc5
ShtTnk <j.1102071016@gmail.com>
2025-10-04 03:03:34 +0900

    added photo upload and view function

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,14 +1,15 @@
 // // src/lib/supabaseClient.ts
-// import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-import { createClient } from "@supabase/supabase-js";
+// // サーバーサイドで RLS を無視したい場合（注意：絶対にクライアントに公開しないこと）
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // ← これで RLS 無視
-);
+// export const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.SUPABASE_SERVICE_ROLE_KEY! // ← これで RLS 無視
+// );
+
