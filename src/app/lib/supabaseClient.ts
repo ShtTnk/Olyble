3c96749c9ad9535fc5b1b3cfe65999d590fd843e
ShtTnk <j.1102071016@gmail.com>
2025-10-04 03:09:26 +0900

    added photo upload and view function

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,10 +1,14 @@
 // // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error("Supabase env variables are missing!")
+}
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 // // サーバーサイドで RLS を無視したい場合（注意：絶対にクライアントに公開しないこと）
 
